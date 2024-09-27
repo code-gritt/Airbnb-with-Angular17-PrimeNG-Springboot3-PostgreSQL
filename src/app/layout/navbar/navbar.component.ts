9385fc3aeda620d36eaadd7038f1cb2c03a765de
gokul <gokulchandan24@gmail.com>
2024-09-28 00:54:38 +0530

    frontend layout done

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ButtonModule } from 'primeng/button';
 import { CategoryComponent } from './category/category.component';
@@ -6,6 +6,7 @@ import { AvatarComponent } from './avatar/avatar.component';
 import { ToolbarModule } from 'primeng/toolbar';
 import { MenuModule } from 'primeng/menu';
 import { MenuItem } from 'primeng/api';
+import { ToastService } from '../toast.service';
 
 @Component({
   selector: 'app-navbar',
@@ -22,14 +23,32 @@ import { MenuItem } from 'primeng/api';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   location = 'Anywhere';
   guests = 'Add guests';
   dates = 'Any week';
 
+  toastService = inject(ToastService);
+
   currentMenuItems: MenuItem[] | undefined = [];
 
   ngOnInit(): void {
-    this.fetchMenu();
+    this.currentMenuItems = this.fetchMenu();
+    this.toastService.send({
+      severity: 'info',
+      summary: 'Welcome to your Airbnb App',
+    });
+  }
+
+  private fetchMenu() {
+    return [
+      {
+        label: 'Sign up',
+        styleClass: 'font-bold',
+      },
+      {
+        label: 'Log in',
+      },
+    ];
   }
 }
